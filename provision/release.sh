b5bb918c9f3d446865cea9e2241a28b3df8369ce
db <daniel.g.barrigas@mindera.com>
2020-02-11 14:15:34 +0000

    provisioning

---
+++ provision/release.sh
@@ -0,0 +1,12 @@
+[ -z $1 ] && echo "missing required argument, usage example: './release.sh 1.0.0'" && exit 1;
+
+echo "Releasing version $1"
+
+git checkout --detach
+npm run build
+git add dist/ --force
+git commit -m $1
+git tag $1
+
+git push --tags
+git checkout master
